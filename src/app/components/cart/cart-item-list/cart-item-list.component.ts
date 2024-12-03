e02a2f126bf61eacedb3c166baf82751ea220d79
Bruno Emanuel Pereira <bruno_emanuel87@hotmail.com>
2024-12-02 21:10:54 -0300

    Cart item finished

--- src/app/components/cart/cart-item-list/cart-item-list.component.ts
+++ src/app/components/cart/cart-item-list/cart-item-list.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Cart } from '../../../models/cart.model';
+import { CartItemComponent } from '../cart-item/cart-item.component';
 
 @Component({
   selector: 'app-cart-item-list',
   standalone: true,
-  imports: [],
+  imports: [
+    CartItemComponent
+  ],
   templateUrl: './cart-item-list.component.html',
   styleUrl: './cart-item-list.component.scss'
 })
 export class CartItemListComponent {
-
+  cart = input.required<Cart>();
 }
--- src/app/components/cart/cart-item/cart-item.component.ts
+++ src/app/components/cart/cart-item/cart-item.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { CartItem } from '../../../models/cart-item.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-cart-item',
   standalone: true,
-  imports: [],
+  imports: [
+    CurrencyPipe
+  ],
   templateUrl: './cart-item.component.html',
   styleUrl: './cart-item.component.scss'
 })
 export class CartItemComponent {
-
+  cartItem = input<CartItem>()
 }
